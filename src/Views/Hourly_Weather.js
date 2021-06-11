4430ff34189ee1b6f90e4bfe5409525b899c4d8c
HafsaZubair17 <80120429+HafsaZubair17@users.noreply.github.com>
2021-06-11 15:28:10 +0500

    Minor Bugs

--- src/Views/Hourly_Weather.js
+++ src/Views/Hourly_Weather.js
@@ -7,28 +7,25 @@ const HourlyWeather = ({ daily_data }) => {
     <>
       {daily_data !== "" ? (
         <div className="daily_display">
-          {daily_data.forecast.forecastday.forEach((dat) => {
-            // <h1>hello</h1>;
-            // console.log(dat);
-            dat.hour.map(
-              (val2) =>
-                // (
-                {
-                  console.log(val2);
-                }
-              //   <div>
-              //     <HourCard
-              //       date={val2.time}
-              //       image={val2.condition.icon}
-              //       temp={val2.condition.text}
-              //       humidity={val2.humidity}
-              //       wind={val2.wind_mph}
-              //       max_temp={val2.temp_f}
-              //       min_temp={val2.temp_c}
-              //     />
-              //   </div>
-              // )
-            );
+          {daily_data.forecast.forecastday.map((dat) => {
+            console.log(dat);
+            return dat.hour.map((val2) => {
+              console.log(val2);
+
+              return (
+                <div>
+                  <HourCard
+                    date={val2.time}
+                    image={val2.condition.icon}
+                    temp={val2.condition.text}
+                    humidity={val2.humidity}
+                    wind={val2.wind_mph}
+                    max_temp={val2.temp_f}
+                    min_temp={val2.temp_c}
+                  />
+                </div>
+              );
+            });
           })}
         </div>
       ) : (
