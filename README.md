1b6f209da07dad3c4b96a5b75b17561fb3347c48
HafsaZubair17 <80120429+HafsaZubair17@users.noreply.github.com>
2021-06-11 08:04:04 +0500

    Readme Added

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # weather_checker
- 
+
+A simple Weathe App using WeatherAPI.com
+
+To set up the app, clone the app in your local github or just download the zip folder.
+To set up the app:
+$ npm install
+To run:
+$ npm start
+To test:
+$ npm test
--- src/Views/Hourly_Weather.js
+++ src/Views/Hourly_Weather.js
@@ -8,19 +8,27 @@ const HourlyWeather = ({ daily_data }) => {
       {daily_data !== "" ? (
         <div className="daily_display">
           {daily_data.forecast.forecastday.forEach((dat) => {
-            dat.hour.map((val2) => (
+            // <h1>hello</h1>;
+            // console.log(dat);
+            dat.hour.map(
+              (val2) =>
+                // (
+                {
+                  console.log(val2);
+                }
               //   <div>
-              <HourCard
-                date={val2.time}
-                image={val2.condition.icon}
-                temp={val2.condition.text}
-                humidity={val2.humidity}
-                wind={val2.wind_mph}
-                max_temp={val2.temp_f}
-                min_temp={val2.temp_c}
-              />
+              //     <HourCard
+              //       date={val2.time}
+              //       image={val2.condition.icon}
+              //       temp={val2.condition.text}
+              //       humidity={val2.humidity}
+              //       wind={val2.wind_mph}
+              //       max_temp={val2.temp_f}
+              //       min_temp={val2.temp_c}
+              //     />
               //   </div>
-            ));
+              // )
+            );
           })}
         </div>
       ) : (
